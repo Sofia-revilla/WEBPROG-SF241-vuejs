6b705981b842b9c07e9d166a03312f7c914fca67
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-20 10:12:53 +0800

    updated task03 import

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,7 +4,7 @@ import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 
-// Keep the CSS for styling
+// Import CSS only
 import './components/css/design.css'
 import './components/css/gallery.css'
 
@@ -13,9 +13,5 @@ const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
-
-// REMOVE THESE LINES:
-// import './components/effect/gallery.js'
-// import './components/effect/script.js'
 
 app.mount('#app')
