e5f6d7751dc21d5ccc2e1c23145c9e2a4493a1ea
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-25 04:49:46 +0800

    Tsk-04

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,29 +1,21 @@
-<template>
-  <main>
-    <ul>
-      <li v-for="instrument in instruments" :key="instrument.id">
-        {{ instrument.name }}
-      </li>
-    </ul>
-  </main>
-</template>
-
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient' 
+import { supabase } from './lib/supabaseClient'
 
 const instruments = ref([])
 
 async function getInstruments() {
-  const { data, error } = await supabase.from('instruments').select()
-  if (error) {
-    console.error('Error fetching:', error)
-  } else {
-    instruments.value = data
-  }
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
 }
 
 onMounted(() => {
-  getInstruments()
+   getInstruments()
 })
 </script>
+
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
