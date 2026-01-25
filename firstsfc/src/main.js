0b289b8a47742f5d8f75ae9486a82cdd13d97ce7
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-25 13:25:15 +0800

    task04

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 
-// Import CSS only
-//import './components/css/design.css'
-//import './components/css/gallery.css'
-
 const app = createApp(App)
-
-app.component('personal-profile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-
-
+app.component('personal-profile', PersonalProfile)
 app.mount('#app')
