dba69308ac5139e2830ba9efc3c570f9701bdd2f
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-25 13:29:49 +0800

    updatw

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,17 +1,49 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
+
 const instruments = ref([])
+
 async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
+  const { data, error } = await supabase
+    .from('instruments')
+    .select('name')
+
+  if (error) {
+    console.error('Fetch Error:', error.message)
+  } else {
+    console.log('Data fetched:', data)
+    instruments.value = data
+  }
 }
+
 onMounted(() => {
-   getInstruments()
+  getInstruments()
 })
 </script>
+
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <div class="list-container">
+    <ul v-if="instruments.length > 0">
+      <li v-for="(instrument, index) in instruments" :key="index">
+        {{ instrument.name }}
+      </li>
+    </ul>
+    <p v-else>No data found or loading...</p>
+  </div>
 </template>
+
+<style scoped>
+.list-container {
+  padding: 40px;
+  font-family: serif;
+}
+ul {
+  list-style-type: disc;
+}
+li {
+  font-size: 1.2rem;
+  margin-bottom: 8px;
+  color: black;
+}
+</style>
