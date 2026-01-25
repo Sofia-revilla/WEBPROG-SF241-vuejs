65f5844ee52a3c37269ed391eb746464d2517183
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-25 13:10:37 +0800

    updatw

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -2,3 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
