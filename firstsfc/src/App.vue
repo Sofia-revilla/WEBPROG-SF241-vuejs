83d561aa5c64210cd64edaf4485b26adf38880b5
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-01-26 15:35:17 +0000

    task-04resolved attempt1

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
+import { supabase } from './lib/supabase'
 const instruments = ref([])
 
 async function getInstruments() {
